6e93b93ff091c9a3d05dd580206ecac4b8a574bc
Arias800 <azertypoil630@gmail.com>
2018-10-31 13:20:46 +0100

    Create service.py

---
+++ enregistrement/service.py
@@ -0,0 +1,23 @@
+import time, os
+from resources.lib.comaddon import progress, addon, xbmc, xbmcgui, VSlog, dialog
+from datetime import datetime
+
+if __name__ == '__main__':
+    ADDON = addon()
+    Debutpath  = os.path.dirname(os.path.realpath(__file__)).replace('\\','/').replace('addons','userdata').replace('plugin.video.vstream','addon_data')
+    path = Debutpath + '/plugin.video.vstream/Enregistrement/'
+    ADDON.setSetting('path_enregistrement_programmation', path)
+    ListeEnregistrement = os.listdir(path)
+    EnregistrementEnCours = False
+    monitor = xbmc.Monitor()
+
+    while not monitor.abortRequested() and not EnregistrementEnCours == True:
+        if monitor.waitForAbort(1):
+            break
+
+        heure = datetime.now().strftime('%H-%M') + '.py'
+        if heure in str(ListeEnregistrement):
+            heure = path + heure
+            VSlog(heure)
+            xbmc.executebuiltin("System.Exec("+(heure)+")")
+            EnregistrementEnCours = True
