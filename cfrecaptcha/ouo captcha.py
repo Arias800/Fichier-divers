3ea5030d3fb1f3079d93061b33a95901033331bf
Arias800 <azertypoil630@gmail.com>
2019-06-05 16:33:09 +0200

    Update ouo captcha.py

--- cfrecaptcha/ouo captcha.py
+++ cfrecaptcha/ouo captcha.py
@@ -4,37 +4,52 @@ from PIL import Image
 import re
 import urllib.request
 import urllib.parse
+import CloudflareScraper
 
+urlOuo = input('Url ouo : ')
+if not '/fbc/' in urlOuo:
+    urlOuo = urlOuo.replace('io/','io/fbc/').replace('press/','press/fbc/')
+print(urlOuo)
+
+UA = ''
+head = {'Host': 'www.google.com',
+'User-Agent': UA,
+'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+'Accept-Language': 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3',
+'Accept-Encoding': 'gzip, deflate, br',
+'DNT': '1',
+'Connection': 'keep-alive',
+'Referer': urlOuo,
+'Upgrade-Insecure-Requests': '1',
+'Pragma': 'no-cache',
+'Cache-Control': 'no-cache',
+'TE': 'Trailers'}
+    
 def ResolveCaptcha(body):
-    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0'}
+    headers = {UA}
+
+    #Recuperer le liens du payload
     captchaScrap = re.findall('value="8"><img class="fbc-imageselect-payload" src="(.+?)"',str(body))
+
+    #Recuperer le texte du captcha
     text = re.search('<div class="rc-imageselect.+?">.+?<strong>(.+?)</strong>',str(body)).group(1)
+
+    #Recuperer les 2 parametre necessaire pour acceder au captcha
     c = re.search('method="POST"><input type="hidden" name="c" value="(.+?)"',str(body)).group(1)
-    print(c)
     k = re.search('k=(.+?)" alt=',str(body)).group(1)
-    print(k)
     params = {
         "c": c,
         "k": k,
     }
     query_string = urllib.parse.urlencode( params )
+
+    #Requete pour le captcha
     url = 'https://www.google.com'+str(captchaScrap[0]) + "?" + query_string
-    print(url)
-
-    file = BytesIO(requests.get(url, headers=headers).content)
-    img = Image.open(file)
-    img.show()
-    response = input('Select all images representing '+text+'. Type from 0 to 8 : ')
-    allNumber = [int(s) for s in response.split() if s.isdigit()]
-    responseFinal = ""
-    for rep in allNumber:
-        responseFinal = responseFinal + '&response='+str(rep)
-
-    print('c='+c+responseFinal)
+    print('\n' + url)
 
     headers = {
         'Host': 'www.google.com',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0',
+        'User-Agent': UA,
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'Accept-Language': 'en-US,en;q=0.5',
         'Accept-Encoding': 'gzip, deflate, br',
@@ -46,25 +61,76 @@ def ResolveCaptcha(body):
         'TE': 'Trailers'
         }
 
+    #Recuperation et ouverture de l'image (uniquement utile pour fonctionner hors Kodi)
+    file = BytesIO(requests.get(url).content)
+    img = Image.open(file)
+    img.show()
+
+    #En attente de la saisie des resultat
+    response = input('Select all images representing '+text+'. Type from 0 to 8 : ')
+
+    #Format la reponse
+    allNumber = [int(s) for s in response.split() if s.isdigit()]
+    responseFinal = ""
+    for rep in allNumber:
+        responseFinal = responseFinal + '&response='+str(rep)
+
+
+    #Requete pour valider le captcha
     r = requests.post(urlBase, data='c='+c+responseFinal, headers=headers)
+
+    #Recupere le token du captcha
     return re.search('<textarea dir="ltr" readonly>(.+?)<',str(r.text)).group(1)
 
+#1er connection pour recuperer la cle du site
+s = requests.Session()
+r = s.get(urlOuo)
+key = re.search('sitekey: "(.+?)"',str(r.text)).group(1)
 
-urlBase  = 'https://www.google.com/recaptcha/api/fallback?k=6LegWQETAAAAAIIaaAhEnrkimbuOF5QJb0ZiYEK7&hl=fr&v=v1558333958099&t=5&ff=true'
+#Pour plus tard
+OuoToken = re.search('<input name="_token" type="hidden" value="(.+?)">',str(r.text)).group(1)
 
-head = {'Host': 'www.google.com',
-'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0',
-'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-'Accept-Language': 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3',
-'Accept-Encoding': 'gzip, deflate, br',
-'DNT': '1',
-'Connection': 'keep-alive',
-'Referer': 'https://ouo.io/fbc/GJRHYi',
-'Cookie': '1P_JAR=2019-06-03-08; NID=184=uJrYqzJzgGe6RZmX8gY_dsl5Xe3EmEh9vAY1eWrtn4nSkj08E-JzZKtuNg2apnaKtAWVi1PV9bMhOVd-krJgfvUD4e-QVV-yMi2-rEEdj5wXOlwj7wSzj0rWlGJydEToe_vYNi9_Z8miOkZkB4-DaOORlha2ZesXwq3P7NvFsGkgj9QDFxHCd45MVO0V3pzx9YEBhmnT_M06NBfxG6kYggArzpfE; CONSENT=YES+FR.fr+20161218-19-0; ANID=OPT_OUT; SID=egc604rtjznXJCIq9vFtTP2cgoYU2JcGOM-viIUjRSLtJEJdWWaxQi_yrZsK67Nu4gXhuA.; HSID=ApkBOVVTshWmTv3ZP; SSID=AopFjfL6iHejpKAZ7; APISID=INXpDeSCPvnjd-Up/A3G4ncpBAY6ITM29W; SAPISID=GfT_-0mPCmAOD2s2/AzPnmL9qR6YSCm8s9; SIDCC=AN0-TYutJx9MkbvWQZRS9-cFmHGVJDqzVDVGlAQ8SEVIP6sXCN0TRsR4r8K1XaJfP7jI_trtIKA; DV=M3mD2hVj1HUTQP_f7OZE3e5NYibHsRY',
-'Upgrade-Insecure-Requests': '1',
-'Pragma': 'no-cache',
-'Cache-Control': 'no-cache',
-'TE': 'Trailers'}
+#Requete vers les serveur de Google pour recuperer le captcha
+urlBase  = 'https://www.google.com/recaptcha/api/fallback?k='+key+'&hl=fr&v=v1558333958099&t=5&ff=true'
 r = requests.get(urlBase, headers=head)
-token = ResolveCaptcha(r.text)
-print('Token final de Recaptcha : '+token)
+
+gToken = ResolveCaptcha(r.text)
+print('Token final de Recaptcha : '+gToken)
+print('\n payload : '+'_token='+OuoToken+'&g-recaptcha-response='+gToken)
+
+#Requete pour valider le captcha du coter de ouo
+url = urlOuo.replace('/fbc/','/go/')
+print(url.split('/')[2])
+params = '_token='+OuoToken+'&g-recaptcha-response='+gToken
+headers = {
+    'Host': url.split('/')[2],
+    'User-Agent': UA,
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Referer': urlOuo,
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Length':str(len(params)),
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'TE': 'Trailers'
+    }
+
+r = requests.post(url,data=params, headers=headers,cookies=s.cookies)
+print(r.headers)
+try:
+    if r.headers['Content-Encoding'] == 'br':
+        import brotli
+        content = brotli.decompress(r.content)
+    else:
+        content = r.text
+except KeyError:
+    content = r.text    
+
+print(content)
+#Recuperer le token et la derniere url de ouo
+final = re.search('<form method="POST" action="(.+?)" accept-charset="UTF-8"><input name="_token" type="hidden" value="(.+?)">',str(content))
+
+#Derniere requete pour recuperer le liens du hoster
+r = requests.post(final.group(1),data='_token='+final.group(2), headers=headers,cookies=s.cookies)
+print("Url final : " + r.url)
