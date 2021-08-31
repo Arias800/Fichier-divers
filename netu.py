67cadfd3c36e3254003a35ea0c6a1deb79a530de
Arias800 <azertypoil630@gmail.com>
2021-08-31 14:09:55 +0200

    Update netu.py

--- netu.py
+++ netu.py
@@ -7,8 +7,11 @@ from resources.lib.parser import cParser
 from resources.lib.comaddon import VSlog
 from resources.lib.tinyjsparser import JsParser
 import re, requests
+import urllib.request
+import gzip
+import json
 
-UA = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:72.0) Gecko/20100101 Firefox/72.0'
+UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
 
 class cHoster(iHoster):
 
@@ -32,7 +35,7 @@ class cHoster(iHoster):
         self.__sUrl = sUrl
 
     def __getIdFromUrl(self):
-        sPattern = 'https*:\/\/hqq\.(?:tv|player|watch)\/player\/embed_player\.php\?vid=([0-9A-Za-z]+)'
+        sPattern = 'https*:\/\/waaw\.(?:tv|player|watch)\/player\/embed_player\.php\?vid=([0-9A-Za-z]+)'
         oParser = cParser()
         aResult = oParser.parse(self.__sUrl, sPattern)
 
@@ -60,63 +63,66 @@ class cHoster(iHoster):
     def __getMediaLinkForGuest(self):
         api_call = ''
 
-        data = {"width":1198,"height":600}
+        headers = { 
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "accept-encoding": "gzip, deflate",
+            "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
+            "cache-control": "no-cache",
+            "pragma": "no-cache",
+            "referer": "https://www.4kstreamz.net/",
+            "sec-ch-ua": 'Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-fetch-dest": "iframe",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "cross-site",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1",
+            "user-agent": UA,
+        }
 
-        headers = {"Host": "hqq.tv",
-            "User-Agent": UA,
-            "Accept": "*/*",
-            "Accept-Language": "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3",
-            "Accept-Encoding": "gzip, deflate",
-            "Referer": self.__sUrl,
-            "Content-Type": "application/json",
-            "X-Requested-With": "XMLHttpRequest",
-            "Origin": "https://hqq.tv",
-            "Connection": "keep-alive",
-            "TE": "Trailers"}
+        req = urllib.request.Request(self.__sUrl, None, headers)
+        with urllib.request.urlopen(req) as response:
+           html = gzip.decompress(response.read()).decode('unicode-escape')
 
-        click_hash = requests.post('https://hqq.tv/player/get_player_image.php', json=data, headers=headers).json()
-    
-        oRequestHandler = cRequestHandler(self.__sUrl)
-        oRequestHandler.addHeaderEntry('User-Agent', UA)
-        html = oRequestHandler.request()
-
-        JScode = re.search(' = null;\s*}(.+?)hcaptchatbl',html, re.MULTILINE|re.DOTALL).group(1).split('//(functio')[0].encode('latin1').decode('utf-8')
+        JScode = re.search(' = null;\s*}(.+?)var',html, re.MULTILINE|re.DOTALL).group(1).encode('latin1').decode('utf-8')
         JP = JsParser()
-        data = (JP.ProcessJS(JScode).encode('latin1').decode('unicode-escape'))
+        data = JP.ProcessJS(JScode).encode('latin1').decode('unicode-escape')
 
         sh = re.search("shh*= *\'(.+?)\';", data).group(1)
         adb = re.search("adbn = \'(.+?)\'", html).group(1)
 
-        data = {"htoken":"","sh":sh,"ver":"4","secure":"0","adb":adb,"v":self.__sUrl.split('?')[1].split('=')[1],"token":"","gt":"","embed_from":"0","wasmcheck":1,"adscore":"","click_hash":click_hash["hash_image"],"clickx":387,"clicky":401}
-        html = requests.post('https://hqq.tv/player/get_md5.php', json=data, headers=headers).json()
-        VSlog(html)
+        data = {"videokey":self.__getIdFromUrl(),"width":693,"height":480}
+        headers["accept"] = "application/json, text/javascript, */*; q=0.01"
+        headers['Content-Type'] = 'application/json'
+        headers['Content-Length'] = len(str(data))
 
-        if int(html["need_captcha"]) == 1:
-            VSlog(self.__sUrl.split('?')[1])
-            captcha = "https://hqq.tv/player/embed_player.php?" + self.__sUrl.split('?')[1] + "&need_captcha=1&pop=0"
+        req = urllib.request.Request('https://waaw.tv/player/get_player_image.php', json.dumps(data).encode('utf-8'), headers)
+        with urllib.request.urlopen(req) as response:
+           html = json.loads(gzip.decompress(response.read()).decode('unicode-escape'))
 
-            oRequestHandler = cRequestHandler(captcha)
-            oRequestHandler.addHeaderEntry('User-Agent', UA)
-            oRequestHandler.addHeaderEntry('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
-            oRequestHandler.addHeaderEntry('Accept-Language', 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3')
-            oRequestHandler.addHeaderEntry('Accept-Encoding', 'gzip')
-            sHtmlContent = oRequestHandler.request()
+        data = {'adb': adb, 'adscore': '', 'click_hash': html['hash_image'], 'clickx': 321, 'clicky': 174, 'embed_from': '0', 'gt': '', 'htoken': '', 'secure': '0', 'sh': sh, 'token': '', 'v': self.__getIdFromUrl(), 'ver': '4', 'wasmcheck': 1}
 
-            from resources.lib import librecaptcha
-            test = librecaptcha.get_token(api_key="6LfCmh4TAAAAAKog9f8wTyEOc0U8Ms2RTuDFyYP_", site_url=captcha,
-                                          user_agent=UA, gui=False, debug=False)
+        headers = {'accept': 'application/json, text/javascript, */*; q=0.01',
+            'accept-encoding': 'gzip, deflate',
+            'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
+            'cache-control': 'no-cache',
+            'content-length': len(str(data)),
+            'content-type': 'application/json',
+            'origin': 'https://waaw.tv',
+            'pragma': 'no-cache',
+            'referer': self.__sUrl,
+            'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': UA,
+            'x-requested-with': 'XMLHttpRequest'}
 
-            oRequestHandler = cRequestHandler(captcha + '&g-recaptcha-response=' + test)
-            oRequestHandler.addHeaderEntry('User-Agent', UA)
-            oRequestHandler.addHeaderEntry('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
-            oRequestHandler.addHeaderEntry('Accept-Language', 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3')
-            oRequestHandler.addHeaderEntry('Accept-Encoding', 'gzip')
-            oRequestHandler.addHeaderEntry('Referer', captcha)
-            sHtmlContent = oRequestHandler.request()
-
-            data = {"htoken":"","sh":sh,"ver":"4","secure":"0","adb":adb,"v":self.__sUrl.split('?')[1].split('=')[1],"token":"","gt":"","embed_from":"0","wasmcheck":1,"adscore":"","click_hash":click_hash["hash_image"],"clickx":387,"clicky":401}
-            html = requests.post('https://hqq.tv/player/get_md5.php', json=data, headers=headers).json()
-            VSlog(html)
+        req = urllib.request.Request('https://waaw.tv/player/get_md5.php', json.dumps(data).encode('utf-8'), headers)
+        with urllib.request.urlopen(req) as response:
+           html = gzip.decompress(response.read())
+           VSlog(html)
 
         if (api_call):
             return True, api_call + '.mp4.m3u8' + '|User-Agent=' + UA
