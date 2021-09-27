47806810e99c3ffdb587b6656ebf854e18cab779
Arias800 <azertypoil630@gmail.com>
2021-09-27 12:30:09 +0200

    Update SubTool.bat

--- SubTool.bat
+++ SubTool.bat
@@ -3,52 +3,68 @@
 chcp 65001
 :Begin
 CLS
-@echo ┌───────────────MENU PRINCIPAL───────────────█
-@echo │Aide contient des informations importantes. █
-@echo │Choix :                                     █
-@echo │ 0. Aide                                    █
-@echo │ 1. Extrait les sous-titres (batch)         █
-@echo │ 2. Convertir sous-titres en .srt (batch)   █
-@echo │ 3. Ajouter les sous-titres au mkv(batch)   █
-@echo │ 4. Quitter                                 █
-@echo └▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
+@echo ┌───────────────MENU PRINCIPAL────────────────█
+@echo │Aide contient des informations importantes.  █
+@echo │Choix :                                      █
+@echo │ 0. Aide                                     █
+@echo │ 1. Extrait les sous-titres (batch)          █
+@echo │ 2. Convertir sous-titres en .srt (batch)    █
+@echo │ 3. Ajouter les sous-titres au mkv(batch)    █
+@echo │ 4. Supprimer une piste de sous-titres(batch)█
+@echo │ 5. Quitter                                  █
+@echo └▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
 
 set /p choice="Choississez l'option voulue : "
 IF %choice% == 0 GOTO Aide
 IF %choice% == 1 GOTO ExtractSub
 IF %choice% == 2 GOTO ConvertSub
 IF %choice% == 3 GOTO MergeSub
-IF %choice% == 4 GOTO End
+IF %choice% == 4 GOTO RemoveTrack
+IF %choice% == 5 GOTO End
 
 
 :ExtractSub
-set /p format="Type de fichier qui comporte les sous-titres ? "
+if exist *.mp4 set format=mp4
+if exist *.mkv set format=mkv
+
 for %%i in (*.%format%) do (
-	ffmpeg -i "%%i" -map 0:s:0  "%%~ni.srt"
+    ffmpeg -i "%%i" -map 0:s:0  "%%~ni.srt"
 )|| pause
 GOTO Begin
 
 
 :ConvertSub
-set /p format="Format des sous-titres a convertir ? "
+if exist *.vtt set format=vtt
+
 for %%i in (*.%format%) do (
-	ffmpeg -i "%%i" -c:s srt  "%%~ni.srt"
+    ffmpeg -i "%%i" -c:s srt  "%%~ni.srt"
 )|| pause
 GOTO Begin
 
 
 :MergeSub
 IF not exist "Final" (mkdir "Final")
+if exist *.mp4 set formatVid=mp4
+if exist *.mkv set formatVid=mkv
 
 if exist *.ass set format=ass
 if exist *.srt set format=srt
 
-echo "Format choisit :" %format%
-
 for %%i in (*.mkv) do (
-	ffmpeg -y -i "%%i" -i "%%~ni."%format% -c:v copy -c:a copy -c:s copy  -map 0:v -map 0:a -map 1 -metadata:s:s:0 language=fre "Final/%%~ni.mkv"
+    ffmpeg -y -i "%%i" -i "%%~ni."%format% -c:v copy -c:a copy -c:s copy  -map 0:v -map 0:a -map 1 -metadata:s:s:0 language=fre "Final/%%~ni."%formatVid%
 )|| pause
 
+GOTO Begin
+
+:RemoveTrack
+IF not exist "Final" (mkdir "Final")
+set /p Num="Numéro de la piste a supprimer ? "
+if exist *.mp4 set format=mp4
+if exist *.mkv set format=mkv
+
+for %%i in (*.%format%) do (
+    ffmpeg -i "%%i" -map 0 -map -0:s:%Num% -c copy "Final/%%~ni."%format%
+)|| pause
 GOTO Begin
 
 
@@ -56,10 +72,11 @@ GOTO Begin
 echo Ce script nécessite FFMPEG, ce dernier doit être inscrit dans les variables d'environnement.
 echo.
 echo Pour ajouter les sous-titres dans les vidéos, il faut que les noms des fichiers sois identiques.
-echo.
 echo Puisque que c'est là-dessus que le script va s'appuyer pour detecter les bons fichiers.
+echo.
+echo Pour supprimer une piste de sous-titres, le numéro 0 est pris en compte.
+echo Pour retirer la piste 1, il faut taper 0. Pour la 2, il faut taper 1...
 echo.
 pause
 GOTO Begin
-
 :End
